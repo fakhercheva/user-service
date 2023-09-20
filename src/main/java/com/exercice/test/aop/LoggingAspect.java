4fa0260a576e6760baa81667c82f153659df0098
fakher mekki <f.mekki@koous-it.com>
2023-09-20 15:27:19 +0200

    add aop package

--- src/main/java/com/exercice/test/aop/LoggingAspect.java
+++ src/main/java/com/exercice/test/aop/LoggingAspect.java
@@ -1,4 +1,41 @@
 package com.exercice.test.aop;
 
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
 public class LoggingAspect {
+    @Around("@annotation(com.exercice.test.annotation.ApiTracking)")
+    public Object  logAccess(ProceedingJoinPoint joinPoint) throws Throwable {
+        long begin = System.currentTimeMillis();
+        log.info("API Access {} with arguments {} ", joinPoint.getSignature().getName(),getAgrs(joinPoint));
+        var response = joinPoint.proceed();
+        long end = System.currentTimeMillis();
+        long  duration = (end-begin);
+        log.info("API output {} after duration of {} milliseconds", joinPoint.getSignature().getName(),duration);
+        return response;
+    }
+
+    public String getAgrs(ProceedingJoinPoint joinPoint)
+    {
+        var ret = new StringBuilder();
+        for(Object o : joinPoint.getArgs())
+        {
+            if(o != null)
+            {
+                if(!ret.isEmpty())
+                {
+                    ret.append(ret).append(",");
+                }
+                ret.append(ret).append(o);
+            }
+        }
+        return ret.toString();
+    }
+
 }
